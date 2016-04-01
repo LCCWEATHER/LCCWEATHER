1534c32086e9429369bf4ba2fbe9689ddf5f49ee
Diego Bustamante <diego_pingy@hotmail.com>
2016-04-01 16:21:56 -0700

    Agregar servidor estacion

--- estacion/servidor.py
+++ estacion/servidor.py
@@ -4,17 +4,11 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return funcionAuxiliar()
-
-if __name__ == '__main__':
-    app.run(debug=True, host='')
 
 def get_newest_data():
     return jsonify(temperatura=g.data.temperatura,
                    concentracionCO=g.data.concentracionCO,
-                   tiempo=g.data.tiempo, 
+                   tiempo=g.data.tiempo,
                    presion=g.data.presion,
                    uv=g.data.uv,
                    humedad=g.data.humedad,
@@ -26,7 +20,7 @@ def get_newest_data():
 def funcionAuxiliar():#todavia no hay componentes para calar esto pero pues en lo que sale
   return jsonify(temperatura=(int)(random.random()*20),
                    concentracionCO=(int)(random.random()*57),
-                   tiempo=datetime.datetime.now(), 
+                   tiempo=datetime.datetime.now(),
                    presion=(int)(random.random()*43),
                    uv=(int)(random.random()*73),
                    humedad=(int)(random.random()*82),
@@ -34,3 +28,9 @@ def funcionAuxiliar():#todavia no hay componentes para calar esto pero pues en l
                    calidadAire="Buena",
                    concentracionNO2=(int)(random.random()*42)
                    )
+
+@app.route('/')
+def index():
+    return funcionAuxiliar()
+if __name__ == '__main__':
+    app.run(debug=True, host='')
