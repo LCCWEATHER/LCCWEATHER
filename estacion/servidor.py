2580724183b190776fb38ed7bfe451f668865645
Fernando Salcido <fsr313@gmail.com>
2016-04-01 15:31:05 -0700

    Agregando objeto de tipo Json

--- estacion/servidor.py
+++ estacion/servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -8,3 +8,14 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='')
+def get_newest_data():#Sujeto a futuros cambios, esta funcion devuelve un objeto de tipo JSON
+    return jsonify(temperatura=g.data.temperatura,
+                   concentracionCO=g.data.concentracionCO,
+                   tiempo=g.data.tiempo, 
+                   presion=g.data.presion,
+                   uv=g.data.uv,
+                   humedad=g.data.humedad,
+                   intensidadLuz=g.data.intensidadLuz,
+                   calidadAire=g.data.calidadAire,
+                   concentracionNO2=g.data.concentracionNO2
+                   )
