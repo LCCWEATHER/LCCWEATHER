6e02bbfea0aa83758d24534c6792f3aeae81bf9d
Rafael Castillo <savoytruffle321@gmail.com>
2016-04-02 15:57:32 -0700

    cambiar clave secreta

--- servidor/app/config.py
+++ servidor/app/config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SECRET_KEY = "ROFLMAOXD"
+SECRET_KEY = "clave secreta"
